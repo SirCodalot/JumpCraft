75656679559b62d3d8584c902ad031c7987c6101
SirCodalot <tzubery.shoham@gmail.com>
2019-05-21 10:52:21 +0300

    Added a custom player class

--- src/main/java/me/codalot/jump/JumpCraft.java
+++ src/main/java/me/codalot/jump/JumpCraft.java
@@ -1,6 +1,24 @@
 package me.codalot.jump;
 
 import me.codalot.core.CodalotPlugin;
+import me.codalot.core.listeners.types.CPlayerListener;
+import me.codalot.core.managers.types.ListenerManager;
+import me.codalot.core.managers.types.PlayerManager;
+import me.codalot.jump.player.JumpPlayer;
+
+import java.util.ArrayList;
 
 public class JumpCraft extends CodalotPlugin {
+
+    @Override
+    public void onEnable() {
+        managers = new ArrayList<>();
+
+        managers.add(new PlayerManager<>(this, JumpPlayer.class));
+        managers.add(new ListenerManager(this,
+                new CPlayerListener(this)
+        ));
+
+        super.onEnable();
+    }
 }
---
+++ src/main/java/me/codalot/jump/player/JumpPlayer.java
@@ -0,0 +1,14 @@
+package me.codalot.jump.player;
+
+import me.codalot.core.files.YamlFile;
+import me.codalot.core.player.CPlayer;
+
+import java.util.UUID;
+
+public class JumpPlayer extends CPlayer {
+
+    public JumpPlayer(UUID uuid, YamlFile file) {
+        super(uuid, file);
+    }
+
+}
