c3f92b78fada3c2f9b6143abd88e7604aa4e3ef6
SirCodalot <tzubery.shoham@gmail.com>
2019-05-21 14:05:34 +0300

    Added double-jump settings

--- src/main/java/me/codalot/jump/managers/SettingsManager.java
+++ src/main/java/me/codalot/jump/managers/SettingsManager.java
@@ -1,13 +1,19 @@
 package me.codalot.jump.managers;
 
+import lombok.Getter;
 import me.codalot.core.files.ResourceFile;
 import me.codalot.core.managers.Manager;
 import me.codalot.jump.JumpCraft;
 
+@Getter
 public class SettingsManager implements Manager {
 
     private JumpCraft plugin;
     private String file;
+
+    private boolean doubleJumpEnabled;
+    private int doubleJumpLimit;
+    private double doubleJumpStrength;
 
     public SettingsManager(JumpCraft plugin, String file) {
         this.plugin = plugin;
@@ -17,6 +23,10 @@ public class SettingsManager implements Manager {
     @Override
     public void load() {
         ResourceFile file = new ResourceFile(plugin, this.file);
+
+        doubleJumpEnabled = file.getBoolean("double-jump.enabled");
+        doubleJumpLimit = file.getInt("double-jump.limit");
+        doubleJumpStrength = file.getInt("double-jump.strength");
     }
 
     @Override
